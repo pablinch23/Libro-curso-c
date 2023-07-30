486d9889265606dd7ca6dec6f749bbae2f1d8801
pablinch23 <140679235+pablinch23@users.noreply.github.com>
2023-07-30 16:53:19 -0600

    Add files via upload

---
+++ Ejemplo3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(void)
+{
+  putchar('H');
+  putchar('O');
+  putchar('L');
+  putchar('A');
+  putchar('\n');
+}
---
+++ Ejemplo4.c
@@ -0,0 +1,11 @@
+/*
+  Imprime el caracter A y un retorno de carro
+*/
+
+#include <stdio.h>
+
+void main(void)
+{
+  putchar(65);
+  putchar('\n');
+}
