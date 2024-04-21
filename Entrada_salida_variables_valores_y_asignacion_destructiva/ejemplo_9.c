69d113cf942195a64937bce5be76289a44b030a2
pablinch23 <140679235+pablinch23@users.noreply.github.com>
2024-04-20 22:59:41 -0600

    Add files via upload

---
+++ Entrada_salida_variables_valores_y_asignacion_destructiva/ejemplo_9.c
@@ -0,0 +1,14 @@
+/*En este ejemplo se le asignara a la letra "a" el caracter 'k' y se imprime
+    A esto se le llama asignacion destructiva*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+    char a = 'k';
+    putchar(a);
+    putchar('\n');
+
+    system("pause");
+}
